721cda6a6c3a219fe4b30462d9af8d04f6d16e1d
Andrew Yang <andrew@Andrews-MacBook-Pro.local>
2021-12-27 19:13:44 -0500

    level 2 - encryption, add environment vars with dot env.

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
